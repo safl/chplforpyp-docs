1775b50a9ded679107cf0e42be6f378c924d5f13
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-12-03 14:01:59 -0800

    Minor python changes to accomodate testing.

--- docs/source/examples/ranges.py
+++ docs/source/examples/ranges.py
@@ -1,2 +1,2 @@
-ns = xrange(1, 10) # yields 1, 2, 3, 4, 5, 6, 7, 8, 9
-ns = xrange(10, 1) # yields nothing
+r1 = xrange(1, 10) # yields 1, 2, 3, 4, 5, 6, 7, 8, 9
+r2 = xrange(10, 1) # yields nothing
